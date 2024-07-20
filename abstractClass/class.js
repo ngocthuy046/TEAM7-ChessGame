7ea9d8c219c37e2d2e9199231a15104c46197cd5
Lê Minh Anh Đoàn <doanle2k1@gmail.com>
2024-07-20 12:05:28 +0700

    SET-297-Đoàn | Add | Abstract Class Board, Player

--- abstractClass/class.js
+++ abstractClass/class.js
@@ -19,3 +19,42 @@ class PieceBase {
     }
     makeMove() {}
 }
+
+class Board {
+  constructor(rows, cols) {
+    this.rows = rows;
+    this.cols = cols;
+  }
+
+  displayBoard() {
+    // Implement logic to display the current state of the game board
+    throw new Error("displayBoard method must be implemented in a concrete class");
+  }
+
+  makeMove(row, col, player) {
+    // Implement logic to make a move on the game board
+    throw new Error("makeMove method must be implemented in a concrete class");
+  }
+
+  checkWin(player) {
+    // Implement logic to check if the given player has won the game
+    throw new Error("checkWin method must be implemented in a concrete class");
+  }
+
+  checkDraw() {
+    // Implement logic to check if the game is a draw
+    throw new Error("checkDraw method must be implemented in a concrete class");
+  }
+}
+
+class Player {
+  constructor(name, symbol) {
+    this.name = name;
+    this.symbol = symbol;
+  }
+
+  makeMove(board) {
+    // Implement logic for the player to make a move on the game board
+    throw new Error("makeMove method must be implemented in a concrete class");
+  }
+}
